f2878ee538a14265e3ae8548374d4836e257985d
Brian Bosh <bbosh@alternateinterior.com>
2012-03-27 15:25:12 -0600

    removed unused code

--- triangle.js
+++ triangle.js
@@ -2,9 +2,6 @@ function Triangle(a, b, c) {
 	this.a = a;
 	this.b = b;
 	this.c = c;
-	this.slopeAB = Math.abs((a[1] - b[1]) / (a[0] - b[0]));
-	this.slopeAC = Math.abs((a[1] - c[1]) / (a[0] - c[0]));
-	this.slopeBC = Math.abs((b[1] - c[1]) / (b[0] - c[0]));
 };
 
 Triangle.prototype.contains = function (x0, y0) {
@@ -13,7 +10,7 @@ Triangle.prototype.contains = function (x0, y0) {
 	    b0 = 0, b1 = 0, b2 = 0, b3 = 0;
 	
 	b0 =  (x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1);
-	b1 = ((x2 - x0) * (y3 - y0) - (x3 - x0) * (y2 - y0)) / b0 ;
+	b1 = ((x2 - x0) * (y3 - y0) - (x3 - x0) * (y2 - y0)) / b0;
 	b2 = ((x3 - x0) * (y1 - y0) - (x1 - x0) * (y3 - y0)) / b0;
 	b3 = ((x1 - x0) * (y2 - y0) - (x2 - x0) * (y1 - y0)) / b0;
 	return b1 > 0 && b2 > 0 && b3 > 0;
