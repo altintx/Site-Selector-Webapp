c6bc04b743febdf14a3818cdf112b33fdbedd464
Brian Bosh <bbosh@alternateinterior.com>
2012-04-27 12:32:13 -0600

    master improvements

--- app/view/LogActionSheet.js
+++ app/view/LogActionSheet.js
@@ -3,34 +3,14 @@ Ext.define('SiteSelector.view.LogActionSheet', {
 	alias: 'widget.LogActionSheet',
 	config: {
 		items: [
-		{
-			text: 'Eat',
-			alias: "addfood",
-			disabled: true
-		},
-		{
-			text: 'Blood Sugar',
-			alias: "addbgnow"
-		},
-		{
-			text: 'Exercise',
-			alias: "addexercise",
-			disabled: true
-		},
-		{
-			text: 'Sick',
-			alias: "addsick",
-			disabled: true
-		},
-		{
-			text: 'Insulin',
-			alias: "addinsulin",
-			disabled: true
-		},
-		{
-			text: 'Cancel',
-			alias: "cancel"
-		}
+			{
+				text: 'Blood Sugar',
+				alias: "addbgnow"
+			},
+			{
+				text: 'Cancel',
+				alias: "cancel"
+			}
 		]
 	}
 });
