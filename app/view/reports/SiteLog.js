83f42f151d453652761550b8a45f9bc1911c2410
Brian Bosh <bbosh@alternateinterior.com>
2012-05-04 08:36:33 -0600

    resolves #18

--- app/view/reports/SiteLog.js
+++ app/view/reports/SiteLog.js
@@ -1,19 +1,18 @@
 Ext.define("SiteSelector.view.reports.SiteLog", {
 	extend: "Ext.dataview.List",
 	config: {
-		itemTpl: new Ext.XTemplate('<table><tr><td rowspan="3"><div style="overflow-y:hidden;height:0.75in;margin-right:0.25in;"><div class="body-thumbnail-{side}" style="top:-{y*3}%;"><div class="circle" style="' + 
+		itemTpl: new Ext.XTemplate('<table><tr><td rowspan="3"><div style="overflow-y:hidden;height:0.65in;margin-right:0.1in;"><div class="body-thumbnail-{side}" style="top:-{y*3.3}%;"><div class="circle site {kind}" style="' + 
 			'left:{x}%;' +
 			'top:{y}%;' +
-			'background:-webkit-linear-gradient(bottom, #4A094A 52%, #8C4FA6 76%);'+
 			'margin-left:-0.125in;' +
 			'margin-top:-0.125in;' +
-			'">+</div></div></div></td><td><strong>{location}</strong> {kind}</td></tr>' +
+			'">+</div></div></div></td><td><strong>{location}</strong> {type}</td></tr>' +
 			'<tr><td>{dates}</td></tr></table>')
 	},
 	
 	constructor: function(config) {
 		config.store = Ext.create("Ext.data.Store", {
-			fields: ["id", "x", "y", "location", "kind", "dates", "side"]
+			fields: ["id", "x", "y", "location", "kind", "dates", "side", "type"]
 		});
 		
 		Ext.data.StoreManager.get("Sites").each(function(site) {
@@ -23,7 +22,8 @@ Ext.define("SiteSelector.view.reports.SiteLog", {
 					side: site.data.side,
 					y: site.data.y * 100,
 					location: site.data.location,
-					kind: site.data.kind == "pump"? "Pump": "CGM",
+					type: site.data.kind == "pump"? "Pump": "CGM",
+					kind: this.data.kind,
 					dates: site.data.when.toLocaleDateString() + " - " + site.data.removed.toLocaleDateString()
 				});
 			} else {
@@ -32,7 +32,8 @@ Ext.define("SiteSelector.view.reports.SiteLog", {
 					x: site.data.x * 100,
 					y: site.data.y * 100,
 					location: site.data.location,
-					kind: site.data.kind == "pump"? "Pump": "CGM",
+					type: site.data.kind == "pump"? "Pump": "CGM",
+					kind: site.data.kind,
 					dates: site.data.when.toLocaleDateString()
 				});
 				
