a0a6af88e2042377b33c7ad873f4404e525dbdfc
Brian Bosh <bbosh@alternateinterior.com>
2012-06-21 14:59:56 -0600

    prevent double clicking icon on homescreen

--- app/view/Homescreen.js
+++ app/view/Homescreen.js
@@ -24,9 +24,10 @@ Ext.define("SiteSelector.view.Homescreen", {
 						text: "Insulin",
 						xtype: "button",
 						ui: "icon",
-						html: '<img src="' + window.location.pathname.replace("index.html", "") + '/resources/icons/homescreen/vial.png" /><div>Insulin</div>',
+						html: ('<img src="' + window.location.pathname.replace("index.html", "") + '/resources/icons/homescreen/vial.png" /><div>Insulin</div>').replace(/\/\//g, '/'),
 						flex: 1,
 						handler: function() {
+							if (this.up("homescreen").screens > 0) return;
 							(function(view) {
 								view.fireEvent("bolus", view);
 							})(this.up("navigationview"))
@@ -37,8 +38,9 @@ Ext.define("SiteSelector.view.Homescreen", {
 						flex: 1,
 						ui: "icon",
 						xtype: "button",
-						html: '<img src="' + window.location.pathname.replace("index.html", "") + '/resources/icons/homescreen/eat.png" /><div>Eat</div>',
+						html: ('<img src="' + window.location.pathname.replace("index.html", "") + '/resources/icons/homescreen/eat.png" /><div>Eat</div>').replace(/\/\//g, '/'),
 						handler: function() {
+							if (this.up("homescreen").screens > 0) return;
 							(function(view) {
 								view.fireEvent("eat", view);
 							})(this.up("navigationview"))
@@ -49,8 +51,9 @@ Ext.define("SiteSelector.view.Homescreen", {
 						text: "Sites",
 						flex: 1,
 						ui: "icon",
-						html: '<img src="' + window.location.pathname.replace("index.html", "") + '/resources/icons/homescreen/sites.png" /><div>Sites</div>',
+						html: ('<img src="' + window.location.pathname.replace("index.html", "") + '/resources/icons/homescreen/sites.png" /><div>Sites</div>').replace(/\/\//g, '/'),
 						handler: function() {
+							if (this.up("homescreen").screens > 0) return;
 							(function(view) {
 								view.fireEvent("show_sites", view);
 							})(this.up("navigationview"))
@@ -68,8 +71,9 @@ Ext.define("SiteSelector.view.Homescreen", {
 						flex: 1,
 						ui: "icon",
 						xtype: "button",
-						html: '<img src="' + window.location.pathname.replace("index.html", "") + '/resources/icons/homescreen/exercise.png" /><div>Exercise</div>',
+						html: ('<img src="' + window.location.pathname.replace("index.html", "") + '/resources/icons/homescreen/exercise.png" /><div>Exercise</div>').replace(/\/\//g, '/'),
 						handler: function() {
+							if (this.up("homescreen").screens > 0) return;
 							(function(view) {
 								view.fireEvent("list_exercises", view);
 							})(this.up("navigationview"))
@@ -78,10 +82,11 @@ Ext.define("SiteSelector.view.Homescreen", {
 					{
 						text: "Log",
 						xtype: "button",
-						html: '<img src="' + window.location.pathname.replace("index.html", "") + '/resources/icons/homescreen/log.png" /><div>Log</div>',
+						html: ('<img src="' + window.location.pathname.replace("index.html", "") + '/resources/icons/homescreen/log.png" /><div>Log</div>').replace(/\/\//g, '/'),
 						flex: 1,
 						ui: "icon",
 						handler: function() {
+							if (this.up("homescreen").screens > 0) return;
 							(function(view) {
 								view.fireEvent("showlog", view);
 							})(this.up("navigationview"))
@@ -92,8 +97,9 @@ Ext.define("SiteSelector.view.Homescreen", {
 						xtype: "button",
 						flex: 1,
 						ui: "icon",
-						html: '<img src="' + window.location.pathname.replace("index.html", "") + '/resources/icons/homescreen/settings.png" /><div>Settings</div>',
+						html: ('<img src="' + window.location.pathname.replace("index.html", "") + '/resources/icons/homescreen/settings.png" /><div>Settings</div>').replace(/\/\//g, '/'),
 						handler: function() {
+							if (this.up("homescreen").screens > 0) return;
 							(function(view) {
 								view.fireEvent("show_settings", view);
 							})(this.up("navigationview"))
@@ -102,5 +108,19 @@ Ext.define("SiteSelector.view.Homescreen", {
 				]
 			}
 		]
+	},
+	
+	constructor: function() {
+		var $this = this;
+		this.callParent(arguments);
+		this.on("painted", function($this) {
+			$this.screens = 0;
+			this.up("navigationview").on("push", function(nv, item) {
+				$this.screens++;
+			})
+			this.up("navigationview").on("pop", function(nv, item) {
+				$this.screens--;
+			})
+		})
 	}
 });
