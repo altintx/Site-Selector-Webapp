18a9ec1f98759d83c9feabb849a15f1c11759e54
Brian Bosh <bbosh@alternateinterior.com>
2012-06-21 15:25:33 -0600

    make site log report handle shots and reverse sort order by default

--- app/view/reports/SiteLog.js
+++ app/view/reports/SiteLog.js
@@ -13,34 +13,44 @@ Ext.define("SiteSelector.view.reports.SiteLog", {
 	},
 	
 	constructor: function(config) {
+		var namemap = {
+			"pump": "Pump",
+			"cgm": "CGM",
+			"shot_bolus": "Bolus Shot",
+			"shot_basal": "Basal Shot"
+		};
+
 		config.store = Ext.create("Ext.data.Store", {
-			fields: ["id", "x", "y", "location", "kind", "dates", "side", "type"]
+			fields: ["id", "x", "y", "location", "kind", "dates", "side", "type", "when"],
+			data: Ext.data.StoreManager.get("Sites").data.items.map(function(site) {
+				if (site.data.removed) {
+					return({
+						x: site.data.x * 100,
+						side: site.data.side,
+						y: site.data.y * 100,
+						location: site.data.location,
+						type: namemap[site.data.kind],
+						kind: site.data.kind,
+						dates: site.data.when.toLocaleDateString() + " - " + site.data.removed.toLocaleDateString(),
+						when: site.data.removed
+					});
+				} else {
+					return({
+						side: site.data.side,
+						x: site.data.x * 100,
+						y: site.data.y * 100,
+						location: site.data.location,
+						type: namemap[site.data.kind],
+						kind: site.data.kind,
+						dates: site.data.when.toLocaleDateString(),
+						when: site.data.when
+					});
+
+				}
+			})
 		});
 		
-		Ext.data.StoreManager.get("Sites").each(function(site) {
-			if (site.data.removed) {
-				config.store.add({
-					x: site.data.x * 100,
-					side: site.data.side,
-					y: site.data.y * 100,
-					location: site.data.location,
-					type: site.data.kind == "pump"? "Pump": "CGM",
-					kind: this.data.kind,
-					dates: site.data.when.toLocaleDateString() + " - " + site.data.removed.toLocaleDateString()
-				});
-			} else {
-				config.store.add({
-					side: site.data.side,
-					x: site.data.x * 100,
-					y: site.data.y * 100,
-					location: site.data.location,
-					type: site.data.kind == "pump"? "Pump": "CGM",
-					kind: site.data.kind,
-					dates: site.data.when.toLocaleDateString()
-				});
-				
-			}
-		})
+		config.store.sort("when", "desc");
 			
 		this.callParent([config]);
 	}
